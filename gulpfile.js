a3b052a05691a80d019cfb516769221880c42934
Shayron Aguiar <shayron.aguiar@gmail.com>
2016-05-17 14:52:46 +0000

    bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 var elixir = require('laravel-elixir');
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
+var bowerDir = 'vendor/bower_components/';
+ 
 elixir(function(mix) {
-    mix.sass('app.scss');
+ mix.copy(bowerDir + 'bootstrap/fonts', 'public/fonts')
+ 
+ .copy(bowerDir + 'jquery/dist/jquery.min.js', 'public/js/jquery.min.js')
+ .copy(bowerDir + 'bootstrap/dist/js/bootstrap.min.js', 'public/js/bootstrap.min.js')
+ 
+ .less('app.less');
 });
